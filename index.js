0256863579e9ec9979c819f34bfa6287e43d2f29
daveloper17 <daveloper17@gmail.com>
2018-06-09 10:53:40 -0600

    added error handler, response message, and Check & Yomama joke functions

--- index.js
+++ index.js
@@ -14,7 +14,79 @@ bot.on('start', () => {
 	
 	bot.postMessageToChannel(
 		'general',
-		"JokeBot is here to make you laugh or cringe!",
+		"type `chuck` or `yomama` or `random` to see a joke",
 		params
 	);
 });
+
+// Error Handler
+bot.on('error', (err) => console.log(err));
+
+// Message Handler
+bot.on('message', data => {
+	if(data.type != 'message')  {
+		return;
+	}
+
+	handleMessage(data.text);
+});
+
+// Respond to Data
+function handleMessage(message) {
+	console.log("handleMessage message", message);
+	if(message.includes(' chuck')) {
+		chuckJoke();
+	} else if(message.includes(' yomama')) {
+		yoMommaJoke();
+	} else if(message.includes(' random')) {
+		randomJoke();
+	}
+}
+
+// tell a Chuck Norris joke
+function chuckJoke() {
+	axios.get('http://api.icndb.com/jokes/random')
+		.then(res => {
+			const joke = res.data.value.joke;
+
+			const params = {
+				icon_emoji: ':laughing:'
+			};
+			
+			bot.postMessageToChannel(
+				'general',
+				`Chuck Norris: ${joke}`,
+				params
+			);
+		});
+}
+
+// Tell a Yo Mama Joke
+function yoMommaJoke() {
+	axios.get('http://api.yomomma.info/')
+		.then(res => {
+			console.log("res", res);
+			const joke = res.data.joke;
+
+			const params = {
+				icon_emoji: ':laughing:'
+			};
+			
+			bot.postMessageToChannel(
+				'general',
+				`Yo Momma: ${joke}`,
+				params
+			);
+		});
+}
+
+// Tell a Random Joke
+function randomJoke() {
+	const rand = Math.floor(Math.random() * 2) + 1;
+console.log("random joke rand", rand);
+	if(rand == 1) {
+		chuckJoke();
+	} else {
+		yoMommaJoke();
+	}
+}
